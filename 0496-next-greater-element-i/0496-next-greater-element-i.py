eacf29bdb73b3ea0c1201d4593cbe00d2a3809e4
kiranmayee09 <kiranmayee0919@gmail.com>
2026-08-05 18:13:17 +0530

    Time: 3 ms (40.2%), Space: 19.4 MB (28.62%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -20,7 +20,7 @@ class Solution:
 
         return answer """
 
-        stack = []
+        """ stack = []
         next_grater = {}
 
         for num in nums2:
@@ -34,5 +34,18 @@ class Solution:
         answer = []
         for num in nums1:
             answer.append(next_grater[num])
-            
-        return answer
+
+        return answer """
+
+        nums1Idx = { n:i for i, n in enumerate(nums1)}
+        res = [-1] * len(nums1)
+
+        for i in range(len(nums2)):
+            if nums2[i] not in nums1Idx:
+                continue
+            for j in range(i + 1, len(nums2)):
+                if nums2[j] > nums2[i]:
+                    idx = nums1Idx[nums2[i]]
+                    res[idx] = nums2[j]
+                    break
+        return res
